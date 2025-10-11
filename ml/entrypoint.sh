e2576142ec02f621c1ec5f3013d7fc0f177d2a69
Valentyn <paladin567@gmail.com>
2025-10-11 17:21:52 +0200

    Fix ml dir created

--- ml/entrypoint.sh
+++ ml/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 MODEL_DIR="ml_data"
 MARKER_FILE="$MODEL_DIR/.models_created"
 
+mkdir -p "$MODEL_DIR"
+
 if [ -f "$MARKER_FILE" ]; then
     echo "Моделі вже створені $(cat $MARKER_FILE)"
     exit 0
